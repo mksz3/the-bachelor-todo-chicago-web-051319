79e70912d7849eff1f425567244fbb9d5ff11c07
Markus Sanders <markus.sanderst@gmail.com>
2019-05-06 18:47:56 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,58 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  # name = ''
+  # contestants = data[season.to_sym]
+  # contestants.each { |contestant|
+  #   if contestant[:status] == "Winner"
+  #     name = contestant[:name]
+  #   end
+  # }
+  # return name.split(' ')[0]
+  
+  data[season].each { |contestant| 
+    if contestant["status"] == "Winner"
+      return contestant["name"].split(' ')[0]
+    end
+  }
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each { |season, contestants|
+    contestants.each { |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    }
+  }
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each { |season, contestants| 
+    contestants.each { |contestant| 
+      if contestant["hometown"] == hometown
+        count += 1
+      end
+    }
+  }
+  count
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each { |season, contestants| 
+    contestants.each { |contestant| 
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    }
+  }
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  avg = 0
+  count = 0
+  data[season].each { |contestant| 
+      avg += (contestant["age"]).to_i
+      count += 1
+  }
+  (avg / count.to_f).round
 end
