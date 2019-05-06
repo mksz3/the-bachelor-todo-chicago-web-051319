756a271de6e62e94ce5fc6ab36bc4f5dfcdb7fc8
Markus Sanders <markus.sanderst@gmail.com>
2019-05-06 18:19:34 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -48,7 +48,7 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  avg = 0
+  avg = 1
   count = 0
   data.each { |season, contestants| 
     contestants.each { |contestant| 
