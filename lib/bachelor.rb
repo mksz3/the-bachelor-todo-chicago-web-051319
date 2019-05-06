8ed9330de5ac7ee70ee0aff788639915d90c6fbc
Markus Sanders <markus.sanderst@gmail.com>
2019-05-06 18:42:19 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -56,7 +56,7 @@ def get_average_age_for_season(data, season)
       count += 1
     }
   }
-  print "avg = " + avg
-  print "count  = " +count 
+  print "avg = " avg
+  print "count  = " count 
   ((avg / count.to_f).round(2)).round(0)
 end
