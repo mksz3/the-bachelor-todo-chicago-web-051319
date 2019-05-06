2062043eb9fa1658b6f5c20628e89d81b405b9fb
Markus Sanders <markus.sanderst@gmail.com>
2019-05-06 18:07:54 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,28 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  # name = ''
+  # contestants = data[season.to_sym]
+  # contestants.each { |contestant|
+  #   if contestant[:status] == "Winner"
+  #     name = contestant[:name]
+  #   end
+  # }
+  # return name.split(' ')[0]
+  
+  data[season].each { |contestant| 
+    if contestant["status"] == "Winner"
+      return contestant["name"].split(' ')[0]
+    end
+  }
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each { |season, contestants|
+    contestants.each { |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    }
+  }
 end
 
 def count_contestants_by_hometown(data, hometown)
@@ -17,3 +36,37 @@ end
 def get_average_age_for_season(data, season)
   # code here
 end
+# { 
+#   "season 30": [
+#     {
+#       "name":      "Beth Smalls",
+#       "age":       "26",
+#       "hometown":  "Great Falls, Virginia",
+#       "occupation":"Nanny/Freelance Journalist",
+#       "status":    "Winner"
+#     },
+#     {
+#       "name":       "Becca Tilley",
+#       "age":        "27",
+#       "hometown":   "Shreveport, Louisiana",
+#       "occupation": "Chiropractic Assistant",
+#       "status":     "Eliminated Week 8"
+#     }
+#   ],
+#   "season 29": [
+#     {
+#       "name":      "Ashley Yeats",
+#       "age":       "24",
+#       "hometown":  "Denver, Colorado",
+#       "occupation":"Dental Assitant",
+#       "status":    "Winner"
+#     },
+#     {
+#       "name":       "Sam Grover",
+#       "age":        "29",
+#       "hometown":   "New York, New York",
+#       "occupation": "Entertainer",
+#       "status":     "Eliminated Week 6"
+#     }
+#   ]
+# }
