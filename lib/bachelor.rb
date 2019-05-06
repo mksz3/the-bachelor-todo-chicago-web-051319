051e2d0e6517eb085c38361e2cd98e2798efaff2
Markus Sanders <markus.sanderst@gmail.com>
2019-05-06 18:22:59 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -52,7 +52,7 @@ def get_average_age_for_season(data, season)
   count = 0
   data.each { |season, contestants| 
     contestants.each { |contestant| 
-      avg += (contestant["age"]).to_i
+      avg += (contestant["age"]).to_f
       count += 1
     }
   }
